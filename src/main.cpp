bd544426e149d0a5cbf5d2fe4038afbbf77b5732
Biel Sala <biel.sala@autonoma.cat>
2023-11-25 16:28:32 +0100

    Add CMakeLists.txt and main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template <typename T>
+void dbgln(T t) {
+  std::cerr << t << std::endl;
+}
+
+template<typename T, typename... Args>
+void dbgln(T t, Args... args) {
+  std::cerr << t;
+  dbgln(args...) ;
+}
+
+int main(void) {
+  return 0;
+}
