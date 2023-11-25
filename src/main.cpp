fbdf5f6ca8e6c2cca10c99950462df1583d5a9bc
Biel Sala <biel.sala@autonoma.cat>
2023-11-25 16:51:58 +0100

    Add first basic options with cxxopts and VERSION constant

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cxxopts.hpp>
 
+const float VERSION = 0.1;
+
 template <typename T>
 void dbgln(T t) {
   std::cerr << t << std::endl;
@@ -12,6 +14,30 @@ void dbgln(T t, Args... args) {
   dbgln(args...) ;
 }
 
-int main(void) {
-  return 0;
+void print_version(void) {
+  std::cout << VERSION << std::endl;
+}
+
+
+int main(int argc, char **argv) {
+  cxxopts::Options options("passman", "Simple password manager");
+
+  options.add_options()
+    ("h,help", "Print usage")
+    ("v,version", "Print version")
+    ("d,debug", "Print debugging info", cxxopts::value<bool>()->default_value("false"))
+    ;
+  auto args = options.parse(argc, argv);
+
+  if (args.count("help")) {
+    std::cout << options.help() << std::endl;
+    exit(EXIT_SUCCESS);
+  } else if (args.count("version")) {
+    print_version();
+    exit(EXIT_SUCCESS);
+  } else {
+    // No arguments were given
+    std::cout << options.help() << std::endl;
+    exit(EXIT_FAILURE);
+  }
 }
